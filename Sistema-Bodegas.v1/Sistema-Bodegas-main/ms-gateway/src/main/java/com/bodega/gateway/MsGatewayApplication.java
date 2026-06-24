4101e6c9b22f411b9f4dd6ded4d87650d0a60954
Da0111zk <da.azocard@duocuc.cl>
2026-06-23 21:38:36 -0400

    Ajuste e implementacion de application.yaml

--- Sistema-Bodegas.v1/Sistema-Bodegas-main/ms-gateway/src/main/java/com/bodega/gateway/MsGatewayApplication.java
+++ Sistema-Bodegas.v1/Sistema-Bodegas-main/ms-gateway/src/main/java/com/bodega/gateway/MsGatewayApplication.java
@@ -3,8 +3,8 @@ package com.bodega.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GatewayApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MsGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
