120ff8ffcc216e82f66cd69ae2eba12d4002b24c
Da0111zk <da.azocard@duocuc.cl>
2026-06-29 04:33:05 -0400

    fix: Ajustes en Application.yaml y Render.yaml

--- Sistema-Bodegas.v1/Sistema-Bodegas-main/ms-proveedores/src/main/java/com/example/ms_proveedores/MsProveedoresApplication.java
+++ Sistema-Bodegas.v1/Sistema-Bodegas-main/ms-proveedores/src/main/java/com/example/ms_proveedores/MsProveedoresApplication.java
@@ -2,8 +2,10 @@ package com.example.ms_proveedores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsProveedoresApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsProveedoresApplication.class, args);
